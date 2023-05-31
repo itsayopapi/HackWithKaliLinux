5f155494d4f6eba9c331e4dbfa7f01efd3a89c97
Papi <111683116+itsayopapi@users.noreply.github.com>
2023-05-31 12:16:31 +0200

    👨‍💻

---
+++ requirements.py
@@ -0,0 +1,7 @@
+import os
+os.system("pip install mechanize")
+os.system("pip install pycrypto")
+os.system("pip install BeautifulSoup")
+os.system("pip install nmap")
+os.system("pip install bs4")
+
